c23af6426c0cfb0ec569f9f8af28d4c58889b85b
vu quang <qvu2k1@gmail.com>
2021-11-25 12:58:27 +0700

    update v1

---
+++ Hash_function/Hash_function/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Hash_function
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbSelection = new System.Windows.Forms.ComboBox();
+            this.cbMD5 = new System.Windows.Forms.CheckBox();
+            this.cbSHA1 = new System.Windows.Forms.CheckBox();
+            this.cbSHA2 = new System.Windows.Forms.CheckBox();
+            this.tbMD5Value = new System.Windows.Forms.TextBox();
+            this.tbSHA1Value = new System.Windows.Forms.TextBox();
+            this.tbSHA2Value = new System.Windows.Forms.TextBox();
+            this.rtbInputData = new System.Windows.Forms.RichTextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data Fomat:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Data:";
+            // 
+            // cbSelection
+            // 
+            this.cbSelection.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbSelection.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbSelection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSelection.FormattingEnabled = true;
+            this.cbSelection.Items.AddRange(new object[] {
+            "Text string",
+            "Hex string",
+            "Binary file"});
+            this.cbSelection.Location = new System.Drawing.Point(19, 61);
+            this.cbSelection.Name = "cbSelection";
+            this.cbSelection.Size = new System.Drawing.Size(94, 24);
+            this.cbSelection.TabIndex = 0;
+            // 
+            // cbMD5
+            // 
+            this.cbMD5.AutoSize = true;
+            this.cbMD5.Location = new System.Drawing.Point(36, 143);
+            this.cbMD5.Name = "cbMD5";
+            this.cbMD5.Size = new System.Drawing.Size(57, 20);
+            this.cbMD5.TabIndex = 2;
+            this.cbMD5.Text = "MD5";
+            this.cbMD5.UseVisualStyleBackColor = true;
+            // 
+            // cbSHA1
+            // 
+            this.cbSHA1.AutoSize = true;
+            this.cbSHA1.Location = new System.Drawing.Point(36, 203);
+            this.cbSHA1.Name = "cbSHA1";
+            this.cbSHA1.Size = new System.Drawing.Size(68, 20);
+            this.cbSHA1.TabIndex = 3;
+            this.cbSHA1.Text = "SHA-1";
+            this.cbSHA1.UseVisualStyleBackColor = true;
+            // 
+            // cbSHA2
+            // 
+            this.cbSHA2.AutoSize = true;
+            this.cbSHA2.Location = new System.Drawing.Point(36, 268);
+            this.cbSHA2.Name = "cbSHA2";
+            this.cbSHA2.Size = new System.Drawing.Size(68, 20);
+            this.cbSHA2.TabIndex = 4;
+            this.cbSHA2.Text = "SHA-2";
+            this.cbSHA2.UseVisualStyleBackColor = true;
+            // 
+            // tbMD5Value
+            // 
+            this.tbMD5Value.Location = new System.Drawing.Point(132, 143);
+            this.tbMD5Value.Name = "tbMD5Value";
+            this.tbMD5Value.ReadOnly = true;
+            this.tbMD5Value.Size = new System.Drawing.Size(523, 22);
+            this.tbMD5Value.TabIndex = 7;
+            this.tbMD5Value.TabStop = false;
+            // 
+            // tbSHA1Value
+            // 
+            this.tbSHA1Value.Location = new System.Drawing.Point(132, 201);
+            this.tbSHA1Value.Name = "tbSHA1Value";
+            this.tbSHA1Value.ReadOnly = true;
+            this.tbSHA1Value.Size = new System.Drawing.Size(523, 22);
+            this.tbSHA1Value.TabIndex = 8;
+            this.tbSHA1Value.TabStop = false;
+            // 
+            // tbSHA2Value
+            // 
+            this.tbSHA2Value.Location = new System.Drawing.Point(132, 266);
+            this.tbSHA2Value.Name = "tbSHA2Value";
+            this.tbSHA2Value.ReadOnly = true;
+            this.tbSHA2Value.Size = new System.Drawing.Size(523, 22);
+            this.tbSHA2Value.TabIndex = 9;
+            this.tbSHA2Value.TabStop = false;
+            // 
+            // rtbInputData
+            // 
+            this.rtbInputData.Location = new System.Drawing.Point(132, 61);
+            this.rtbInputData.Name = "rtbInputData";
+            this.rtbInputData.Size = new System.Drawing.Size(523, 24);
+            this.rtbInputData.TabIndex = 1;
+            this.rtbInputData.Text = "";
+            this.rtbInputData.TextChanged += new System.EventHandler(this.rtbInputData_TextChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(552, 332);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(103, 40);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(552, 91);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(103, 30);
+            this.btnSelectFile.TabIndex = 11;
+            this.btnSelectFile.Text = "Browse";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 420);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.rtbInputData);
+            this.Controls.Add(this.tbSHA2Value);
+            this.Controls.Add(this.tbSHA1Value);
+            this.Controls.Add(this.tbMD5Value);
+            this.Controls.Add(this.cbSHA2);
+            this.Controls.Add(this.cbSHA1);
+            this.Controls.Add(this.cbMD5);
+            this.Controls.Add(this.cbSelection);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbSelection;
+        private System.Windows.Forms.CheckBox cbMD5;
+        private System.Windows.Forms.CheckBox cbSHA1;
+        private System.Windows.Forms.CheckBox cbSHA2;
+        private System.Windows.Forms.TextBox tbMD5Value;
+        private System.Windows.Forms.TextBox tbSHA1Value;
+        private System.Windows.Forms.TextBox tbSHA2Value;
+        private System.Windows.Forms.RichTextBox rtbInputData;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnSelectFile;
+    }
+}
+
---
+++ Hash_function/Hash_function/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Hash_function
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.cbSelection.SelectedIndexChanged += new EventHandler(onSelectionChange);
+            this.btnSelectFile.Enabled = false;
+
+            cbMD5.Checked = true;
+            cbSHA1.Checked = true;
+            cbSHA2.Checked = true;
+        }
+
+        private void onSelectionChange(object sender, EventArgs e)
+        {
+            var select = cbSelection.SelectedIndex;
+            if (select == 0)
+            {
+                btnSelectFile.Enabled = false;
+                rtbInputData.Text = "";
+                
+                tbMD5Value.Text = "";
+                tbSHA1Value.Text = "";
+                tbSHA2Value.Text = "";
+            }
+            else if (select == 1)
+            {
+                btnSelectFile.Enabled = false;
+                rtbInputData.Text = "";
+                
+                tbMD5Value.Text = "";
+                tbSHA1Value.Text = "";
+                tbSHA2Value.Text = "";
+            }
+            else if (select == 2)
+            {
+                btnSelectFile.Enabled = true;
+                
+                tbMD5Value.Text = "";
+                tbSHA1Value.Text = "";
+                tbSHA2Value.Text = "";
+
+            }
+            
+
+        }
+
+        
+        public void MD5Hash(StringBuilder MD5Value)
+        {
+            var data = rtbInputData.Text;
+            MD5 mh = MD5.Create();
+            byte[] dataInput = System.Text.Encoding.ASCII.GetBytes(data);
+            byte[] hash = mh.ComputeHash(dataInput);
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                MD5Value.Append(hash[i].ToString("x2"));
+            }
+            tbMD5Value.Text = MD5Value.ToString();
+        }
+
+        public void SHA1Hash(StringBuilder SHA1Value)
+        {
+            var data = rtbInputData.Text;
+            SHA1Managed sHA1 = new SHA1Managed();
+            byte[] dataInput = System.Text.Encoding.ASCII.GetBytes(data);
+            byte[] hash = sHA1.ComputeHash(dataInput);
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                SHA1Value.Append(hash[i].ToString("x2"));
+            }
+            tbSHA1Value.Text = SHA1Value.ToString();
+        }
+
+        public void SHA2Hash(StringBuilder SHA2Value)
+        {
+            var data = rtbInputData.Text;
+            SHA256Managed sHA256 = new SHA256Managed();
+            byte[] dataInput = System.Text.Encoding.ASCII.GetBytes(data);
+            byte[] hash = sHA256.ComputeHash(dataInput);
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                SHA2Value.Append(hash[i].ToString("x2"));
+            }
+            tbSHA2Value.Text = SHA2Value.ToString();
+        }
+
+        public bool Check(char[] dataInput, char[] word)
+        {
+            foreach (var item in word)
+            {
+                for (int j = 0; j < dataInput.Length; j++)
+                {
+                    if (dataInput[j] != word[j])
+                        return true;
+                }
+            }
+            return false;
+        }
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            StringBuilder MD5Value = new StringBuilder();
+            StringBuilder SHA1Value = new StringBuilder();
+            StringBuilder SHA2Value = new StringBuilder();
+            
+            var comboxValue = cbSelection.SelectedItem as string;
+
+            if (comboxValue == "Hex string")
+            {
+                var dataInput = rtbInputData.Text;
+                if (dataInput.Length % 2 != 0)
+                {
+                    MessageBox.Show("Warning: The Length hex string mush be even!!!");
+                    tbMD5Value.Text = "";
+                    tbSHA1Value.Text = "";
+                    tbSHA2Value.Text = "";
+                }
+            }
+            var selection = cbSelection.SelectedItem as String;
+            var character = new char[12]
+            {
+                'A', 'B', 'C', 'D', 'E', 'F',
+                'a', 'b', 'c', 'd', 'e', 'f'
+            };
+            if (selection == "Hex string")
+            {
+                var dataInput = rtbInputData.Text;
+                var temp = new char[character.Length];
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    temp[i] = character[i];
+                }
+                int flag = 0;
+                for (int i = 0; i < dataInput.Length; i++)
+                {
+                    if (Check(temp, character) == true)
+                    {
+                        MessageBox.Show("Warning: The hex string includes invalid symbols(s)!!!");
+                        cbMD5.Checked = false;
+                        cbSHA1.Checked = false;
+                        cbSHA2.Checked = false;
+                        tbMD5Value.Text = "";
+                        tbSHA1Value.Text = "";
+                        tbSHA2Value.Text = "";
+                        flag = 1;
+                    }
+                }
+                if (flag == 0)
+                {
+                    cbMD5.Checked = true;
+                    cbSHA1.Checked = true;
+                    cbSHA2.Checked = true;
+                    MD5Hash(MD5Value);
+                    SHA1Hash(SHA1Value);
+                    SHA2Hash(SHA2Value);
+
+                }
+
+            }
+            else if (selection == "Text string")
+            {
+                if (cbMD5.Checked == true)
+                {
+                    MD5Hash(MD5Value);
+                    
+                }
+                else
+                {
+                    tbMD5Value.Text = "";
+                }
+
+                if (cbSHA1.Checked == true)
+                {
+                    SHA1Hash(SHA1Value);
+                }
+                else
+                {
+                    tbSHA1Value.Text = "";
+                }
+                if (cbSHA2.Checked == true)
+                {
+                    SHA2Hash(SHA2Value);
+
+                }
+                else
+                {
+                    tbSHA2Value.Text = "";
+                }
+            }
+            else
+            {
+                MD5Hash(MD5Value);
+                SHA1Hash(SHA1Value);
+                SHA2Hash(SHA2Value);
+            }
+            
+
+        }
+
+        private void rtbInputData_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            string txtEditor;
+            ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            //ofd.Filter = "Text files (*.txt)|*.txt";
+            StreamReader reader;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtEditor = ofd.FileName.ToString();
+                reader = new StreamReader(txtEditor);
+                var filterReader = reader.ReadToEnd();
+                
+               /* for (int i = 0; i < filterReader.Length; i++)
+                {
+
+                }*/
+               /* for (int i = 0; i < txtEditor.Length; i++)
+                {
+                    if ([i] != 0 || tmp[i] != 1)
+                    {
+                        MessageBox.Show("Warning: File must be a binary file!!!");
+                    }
+                }*/
+                rtbInputData.Text = filterReader.ToString();
+            }
+        }
+    }
+}
